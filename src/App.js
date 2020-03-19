97eefc35fe4256ea6d38bf9e9bae4c864c803aaf
Kulakova-Anhelina <kulakovaanhelina@gmail.com>
2020-03-19 14:27:59 +0200

    styling app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import Carlist from "./components/Carlist";
 import "./App.css";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 
 function App() {
   return (
     <div className="App">
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+            Car Shop
+          </Typography>
+        </Toolbar>
+      </AppBar>
       <Carlist />
     </div>
   );
